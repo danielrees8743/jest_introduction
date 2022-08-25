ea918b3a45bf39fd8a3976be852a067a2d8b97bf
StephanieRuthJones <stephanieruthjones@gmail.com>
2022-08-25 15:13:34 -0600

    Add sum and multiply functions to math.js file

---
+++ math.js
@@ -0,0 +1,14 @@
+function sumTwoNums(numOne, numTwo) {
+    return numOne + numTwo
+}
+
+function multiplyNums(...args) {
+    const product = args.reduce((acc, number) => {
+        console.log(acc, number)
+        acc = acc *= number
+        return acc
+    }, 1)
+    return product
+}
+
+module.exports = {sumTwoNums, multiplyNums}
